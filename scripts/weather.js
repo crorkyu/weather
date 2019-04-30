455c3633bd905edaa0923c0f7706fa5ad75a0ce7
crore <48863593+crorkyu@users.noreply.github.com>
2019-04-30 17:06:19 -0400

    initial commit

---
+++ scripts/weather.js
@@ -0,0 +1,21 @@
+const key = "O8ap8hEaEK91Vd5kQQDJ9gEk3ISabHFg";
+
+const getWeather = async info => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${info}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+const getCity = async city => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
